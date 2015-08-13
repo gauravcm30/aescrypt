f7cca2ef5c9342b4b9657a23210e414c1cb4fd1b
gauravcm30 <gauravcm30@gmail.com>
2015-08-13 12:55:56 +0530

    Update aescrypt.rb
    128 bit changes

--- lib/aescrypt.rb
+++ lib/aescrypt.rb
@@ -31,12 +31,12 @@ require 'base64'
 
 module AESCrypt
   def self.encrypt(message, password)
-    Base64.encode64(self.encrypt_data(message.to_s.strip, self.key_digest(password), nil, "AES-256-CBC"))
+    Base64.encode64(self.encrypt_data(message.to_s.strip, self.key_digest(password), nil, "AES-128-CBC"))
   end
 
   def self.decrypt(message, password)
     base64_decoded = Base64.decode64(message.to_s.strip)
-    self.decrypt_data(base64_decoded, self.key_digest(password), nil, "AES-256-CBC")
+    self.decrypt_data(base64_decoded, self.key_digest(password), nil, "AES-128-CBC")
   end
 
   def self.key_digest(password)
